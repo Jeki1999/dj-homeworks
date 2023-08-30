6e2764d58e0e4588618e78bd5d5ae99210fc1fbf
Evgeniy Veselskiy <jeka.vesel@mail.ru>
2023-08-30 13:49:10 +0600

    выполнено 3 задание(телефоны)

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,3 +1,4 @@
+from .models import Phone
 from django.shortcuts import render, redirect
 
 
@@ -6,12 +7,42 @@ def index(request):
 
 
 def show_catalog(request):
+
+    sort_order_param = request.GET.get('sort', 'name')
+
+    Phone.objects.all()
+    if sort_order_param == 'name':
+        sort_order ='name'
+    elif sort_order_param == 'min_price':
+        sort_order ='price'
+    else:
+        sort_order ='-price'
+
+    phone_objects = Phone.objects.order_by(sort_order)
+
     template = 'catalog.html'
-    context = {}
+    context = {
+        'phones': phone_objects,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone_product = Phone.objects.filter(slug=slug)
+
+    phones_list = list(phone_product)
+
+    phone = {
+        "name": phones_list[0].name,
+        "price": phones_list[0].price,
+        "image": phones_list[0].image_url,
+        "release_date": phones_list[0].release_date,
+        "lte_exist": phones_list[0].lte_exist,
+        "slug": phones_list[0].slug
+    }
+
     template = 'product.html'
-    context = {}
+    context = { "phone": phone }
+    print(context)
+
     return render(request, template, context)
